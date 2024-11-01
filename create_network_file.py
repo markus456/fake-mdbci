6d2ebeb86489259daa09dc20e7a17198c10021bc
Markus Mäkelä <markus456@gmail.com>
2024-11-01 10:02:01 +0200

    Create configuration only for running VMs
    The `vagrant ssh-config` will fail of one of the VMs is not running. To
    avoid having to keep all of the VMs running, the network configuration
    script can query the set of running VMs first and only generate the
    configuration for those.
    
    Also fixed the hostnames for the node and galera type VMS to be
    valid. Previously it used underscores which wasn't what was actually
    used on the VM.

--- create_network_file.py
+++ create_network_file.py
@@ -4,8 +4,6 @@ import argparse
 import subprocess
 import os
 
-output = subprocess.run(["vagrant", "ssh-config"], cwd="./develop/", capture_output=True).stdout
-
 translate = {
     "User": "whoami",
     "IdentityFile": "keyfile",
@@ -13,17 +11,28 @@ translate = {
     "Host": "hostname"
 }
 
+vms = []
+output = subprocess.run(["vagrant", "status"], cwd="./develop/", capture_output=True).stdout
+
+for line in output.decode().split(os.linesep):
+    if "running" in line:
+        vms.append(line.split(" ")[0])
+
 with open("develop_network_config", "w") as f:
     f.write("[__anonymous__]\n")
-    for line in [s.strip() for s in output.decode().split(os.linesep)]:
-        print(line)
-        (key, sep, val) = line.partition(" ")
-        if key == "Host":
-            host = val
-            # We need to change these, otherwise the hostnames are wrong.
-            if val == "maxscale_000":
-                val = "maxscale"
-            elif val == "maxscale_001":
-                val = "maxscale2"
-        if key in translate:
-            f.write("%s_%s = %s\n" % (host, translate[key], val))
+    for vm in vms:
+        output = subprocess.run(["vagrant", "ssh-config", vm], cwd="./develop/", capture_output=True).stdout
+        for line in [s.strip() for s in output.decode().split(os.linesep)]:
+            print(line)
+            (key, sep, val) = line.partition(" ")
+            if key == "Host":
+                host = val
+                # We need to change these, otherwise the hostnames are wrong.
+                if val == "maxscale_000":
+                    val = "maxscale"
+                elif val == "maxscale_001":
+                    val = "maxscale2"
+                else:
+                    val = val.replace("_", "-")
+            if key in translate:
+                f.write("%s_%s = %s\n" % (host, translate[key], val))
