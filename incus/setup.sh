bc2d38348d9646d36203a0f68d804f3aaf12bfa8
Markus Mäkelä <markus456@gmail.com>
2025-11-28 14:02:53 +0200

    Add scripts for setups using Incus
    System containers are an extremely light approach that provide a near
    identical experience to full virtualization via Vagrant. The benefits of
    this are similar to application containers (i.e. docker) where only as
    much memory is used as is needed.
    
    This approach enables dense packing of test setups and makes parallel
    testing possible.
    
    One downside is that installing docker inside of a system container also
    exposes the docker network as one of the IP addresses of the
    container. This currently confuses the scripts so any tests that need
    docker won't work.

--- incus/setup.sh
+++ incus/setup.sh
@@ -52,15 +52,9 @@ incus exec ${setup}-maxscale-000 bash <<EOF
 mkdir -p /build
 cd /build
 
-# Workarounds for some Rocky 8 problems...
-dnf -y install libasan libubsan
+# Workarounds for some Rocky 8 problems. The testing also requires that Java is installed on the MaxScale VM.
+dnf -y install libasan libubsan java-latest-openjdk
 source /opt/rh/gcc-toolset-11/enable
-
-# Install docker, needed by a few tests
-dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-systemctl --now enable docker
-sudo usermod -a -G docker vagrant
 
 git config --global --add safe.directory /MaxScale
 cmake /MaxScale/ -DPACKAGE=Y -DCMAKE_BUILD_TYPE=Debug -DWITH_ASAN=Y -DWITH_UBSAN=Y
