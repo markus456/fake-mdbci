40a8363de19e1e281256d146146e408c4ef63048
Markus Mäkelä <markus456@gmail.com>
2025-12-02 08:14:43 +0200

    Add helper script for setting up multiple VMs

---
+++ lxc/setup_for_parallel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "USAGE: LIST_OF_VMS"
+    echo ""
+    echo "The first VM in the list is built and the rest are copied from it."
+    exit 1
+fi
+
+scriptdir=$(dirname $(realpath $0))
+$scriptdir/setup.sh $1
+$scriptdir/copy_setup.sh $@
+$scriptdir/create_resource.sh $@
