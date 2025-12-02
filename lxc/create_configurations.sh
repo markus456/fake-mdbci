ad550f9a26f71ea67aa9a6d837d26fdd83e6f536
Markus Mäkelä <markus456@gmail.com>
2025-12-02 15:14:41 +0200

    Move config creation to separate script
    The script can be used in a standalone mode to recreate the network
    configurations if the containers have to be recreated when
    e.g. rebuilding MaxScale. The create_resource.sh script was combined
    into it as it makes sense to do it in there as well.

---
+++ lxc/create_configurations.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+. $(dirname $(realpath $0))/utils.sh
+
+for setup in "$@"
+do
+    echo "[__anonymous__]" > ${setup}_network_config
+
+    for name in maxscale-00{0,1} node-00{0..3} galera-00{0..3}
+    do
+        wait_for_network ${setup}-${name}
+        line=$($lxc_cmd ls -f csv ${setup}-${name})
+        ip=$(echo $line|cut -f 3 -d ,|sed 's/ .*//')
+        confname=$(echo ${name}|tr '-' '_')
+        cat <<EOF >> ${setup}_network_config
+${confname}_whoami=vagrant
+${confname}_hostname=${name}
+${confname}_network=$ip
+${confname}_keyfile=$PWD/ssh_key
+EOF
+        cat <<EOF >> ${setup}_ssh_config
+Host $confname
+  HostName $ip
+  User vagrant
+  Port 22
+  UserKnownHostsFile /dev/null
+  StrictHostKeyChecking no
+  PasswordAuthentication no
+  IdentityFile $PWD/ssh_key
+  IdentitiesOnly yes
+  LogLevel FATAL
+
+EOF
+    done
+
+    echo "GALERA_BACKEND,MAXSCALE,REPL_BACKEND,SECOND_MAXSCALE" > ${setup}_configured_labels
+done
+
+
+function list-envs() {
+    for id in $@
+    do
+        echo "{\"id\":\"$id\"}"
+    done
+}
+
+cat <<EOF|jq . > resource-spec.json
+{
+  "version": {
+    "major": 1,
+    "minor": 0
+  },
+  "local": [
+    {
+      "envs": [
+        $(list-envs "$@"|paste -s -d,)
+      ]
+    }
+  ]
+}
+EOF
